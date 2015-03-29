f22f25bef496e9a4e268ccf70ca022bac80083e4
tochman <thomas@necebo.se>
2015-03-29 18:14:43 +0200

    WIP mobile UI: fixed asset loading to exclude mobile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,6 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require shoppe/stripe/form_handler
-//= require_tree .
+//=
+//= require_directory .
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,7 +28,7 @@ class ApplicationController < ActionController::Base
   
   def mobile_device?
     if session[:mobile_param]
-      session[:mobile_param] == "1"
+      session[:mobile_param] == '1'
     else
       request.user_agent =~ /Mobile|webOS/
     end
