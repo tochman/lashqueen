75ea4c3fd347add662bdd90953033b58ad9fd76e
Thomas Ochman <thomas@nocebo.se>
2015-03-29 21:10:56 +0200

    WIP mobile UI: added method to recognize mobile browsers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,7 +30,9 @@ class ApplicationController < ActionController::Base
     if session[:mobile_param]
       session[:mobile_param] == '1'
     else
-      request.user_agent =~ /Mobile|webOS/
+     # request.user_agent =~ /Mobile|webOS/
+      request.user_agent =~ /android|blackberry|iphone|ipad|ipod|iemobile|mobile|webos/i
+      
     end
   end
   helper_method :mobile_device?
